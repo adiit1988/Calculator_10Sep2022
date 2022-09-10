09f2df217905a8f933f6f6c30614dbd0d3ff37b6
adiit1988 <adi.it1988@gmail.com>
2022-09-10 23:17:31 +0530

    Update IResult.java
    added with public void profile(); API

--- Calculator/src/com/calculator/result/IResult.java
+++ Calculator/src/com/calculator/result/IResult.java
@@ -4,5 +4,9 @@ public interface IResult {
 	public int result();
 	public void init();
 	public void display();
+<<<<<<< HEAD
 	public void hide();
+=======
+	public void profile();
+>>>>>>> cd251e3 (Update IResult.java)
 }
